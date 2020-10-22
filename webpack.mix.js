2d49153a88a6ecc9513d7e6eb8ccb915f34d8751
karlhillx <karlhillx@gmail.com>
2020-10-21 22:52:48 -0400

    Update latest Laravel files.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+require('mix-tailwindcss');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,7 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
