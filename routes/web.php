bfe1413a7785d789e85a505d31b1ae84f33c6a05
karlhillx <karlhillx@gmail.com>
2021-02-19 08:15:31 -0500

    Update dependencies.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\WelcomeController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 Route::post('portfolio', [WelcomeController::class, 'portfolio'])->name('portfolio');
 
-Auth::routes();
+//Auth::routes(['register' => false]);
