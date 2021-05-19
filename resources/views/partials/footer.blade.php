c05b582f6df3531c0be205b790ea8a3b1b61814b
karlhillx <karlhillx@gmail.com>
2021-05-18 22:21:27 -0400

    Fix footer.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,34 +1,32 @@
 <!-- Footer -->
 <footer class="bg-black">
-    <div class="container mx-auto sm:px-4 pt-12 pb-12 py-6 px-6">
-        <div class="flex flex-wrap">
-            <div class="grid grid-cols-3 gap-4">
-                <div class="text-gray-400">© {{ date('Y') }} Karl Hill. Laravel Build v{{ app()->version() }}.<br>
-                    Unless otherwise indicated, content is licensed under the Creative Commons Attribution
-                    4.0 International.
-                </div>
-                <div>&nbsp;</div>
-                <div>
-                    <div class="bottom-social-icons -align-right">
-                        <a class="linkedin" target="_blank" href="https://linkedin.com/in/khill">
-                            <i class="fab fa-linkedin"></i>
-                        </a>
-                        <a class="github" target="_blank" href="https://github.com/karlhillx/">
-                            <i class="fab fa-github"></i>
-                        </a>
-                        <a class="stackoverflow" target="_blank"
-                           href="https://stackoverflow.com/users/633440/karl-hill">
-                            <i class="fab fa-stack-overflow"></i>
-                        </a>
-                        <a class="twitter" target="_blank" href="https://twitter.com/karl_hill/">
-                            <i class="fab fa-twitter-square"></i>
-                        </a>
-                        <a class="discogs" target="_blank" href="https://www.discogs.com/artist/1286669-Karl-Hill">
-                            <i class="fas fa-compact-disc"></i>
-                        </a>
-                    </div>
-                </div>
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div class="flex justify-center space-x-6 md:order-2">
+            <div class="bottom-social-icons -align-right">
+                <a class="linkedin" target="_blank" href="https://linkedin.com/in/khill">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a class="github" target="_blank" href="https://github.com/karlhillx/">
+                    <i class="fab fa-github"></i>
+                </a>
+                <a class="stackoverflow" target="_blank"
+                   href="https://stackoverflow.com/users/633440/karl-hill">
+                    <i class="fab fa-stack-overflow"></i>
+                </a>
+                <a class="twitter" target="_blank" href="https://twitter.com/karl_hill/">
+                    <i class="fab fa-twitter-square"></i>
+                </a>
+                <a class="discogs" target="_blank" href="https://www.discogs.com/artist/1286669-Karl-Hill">
+                    <i class="fas fa-compact-disc"></i>
+                </a>
             </div>
+        </div>
+        <div class="mt-8 md:mt-0 md:order-1">
+            <p class="text-sm text-gray-400">
+                © {{ date('Y') }} Karl Hill. Laravel Build v{{ app()->version() }}.<br>
+                Unless otherwise indicated, content is licensed under the Creative Commons Attribution
+                4.0 International.
+            </p>
         </div>
     </div>
 </footer>
