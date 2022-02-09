efaa6696130acf05aa907d910309ca1ba7364de7
Karl Hill <karl.hill@nasa.gov>
2022-02-09 16:06:13 -0500

    Update to Laravel 9.0.1

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -11,6 +14,6 @@
 |
 */
 
-/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});*/
+});
