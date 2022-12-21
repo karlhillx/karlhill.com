fe3520763f5925bde4329736c4b139bcaa97c0f6
Karl Hill <karl.hill@nasa.gov>
2022-12-20 22:56:26 -0500

    Add new blog post.

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -125,6 +125,37 @@
                     <div class="mx-auto max-w-2xl lg:max-w-5xl">
                         <div class="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
                             <div class="flex flex-col gap-16">
+
+
+                                <article class="group relative flex flex-col items-start">
+                                    <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                                        <div
+                                            class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
+                                        <a href="https://laravel-livewire.com/" target="_blank"><span
+                                                class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span><span
+                                                class="relative z-10">Livewire + Inertia</span></a>
+                                    </h2>
+                                    <time
+                                        class="relative z-10 order-first mb-3 flex items-center text-sm text-orange-400 dark:text-zinc-500 pl-3.5"
+                                        datetime="2022-07-14"><span class="absolute inset-y-0 left-0 flex items-center"
+                                                                    aria-hidden="true"><span
+                                                class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span></span>December
+                                        20, 2022
+                                    </time>
+                                    <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                                        If you're a Laravel developer, you may be interested in using Laravel Livewire
+                                        and Inertia to build interactive and dynamic web applications. Livewire is a
+                                        full-stack framework that allows you to create dynamic, reactive components
+                                        using PHP. In contrast, Inertia enables you to use those components on the front
+                                        end with minimal JavaScript. Together, these tools can make building modern,
+                                        responsive web applications with minimal code easier. Livewire and Inertia are
+                                        easy to learn and use, and they can be a great addition to your toolkit as a
+                                        Laravel developer. Give them a try and see how they can improve your workflow.
+                                    </p>
+
+                                </article>
+
+
                                 <article class="group relative flex flex-col items-start">
                                     <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
                                         <a href="https://laravel-vite.dev/" target="_blank">
@@ -145,6 +176,9 @@
                                         5, 2022
                                     </time>
                                     <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                                        Are you tired of dealing with the slow build times and complicated configuration
+                                        of Laravel Mix? If so, it might be time to consider transitioning to Vite.
+
                                         Vite is a modern frontend build tool that provides an extremely fast development
                                         environment and bundles your code for production.
 
@@ -156,6 +190,34 @@
                                         Blade directives to load your assets for development and production.
                                     </p>
                                 </article>
+
+                                <article class="group relative flex flex-col items-start">
+                                    <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                                        <div
+                                            class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
+                                        <a href="https://tailwindcss.com/" target="_blank"><span
+                                                class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span><span
+                                                class="relative z-10">Rewriting interfaces with TailwindCSS</span></a>
+                                    </h2>
+                                    <time
+                                        class="relative z-10 order-first mb-3 flex items-center text-sm text-orange-400 dark:text-zinc-500 pl-3.5"
+                                        datetime="2022-07-14"><span class="absolute inset-y-0 left-0 flex items-center"
+                                                                    aria-hidden="true"><span
+                                                class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span></span>February
+                                        14, 2022
+                                    </time>
+                                    <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                                        If you're tired of the same old UI designs and want to give your interface a
+                                        fresh, modern look, consider rewriting it with TailwindCSS.
+                                        When I first came across <a href="https://tailwindcss.com/" target="_blank">TailwindCSS</a>,
+                                        I was amazed by the framework's simplicity. I rewrote my interfaces in a matter
+                                        of minutes with efficiency and maintainability, leaving me more time to focus on
+                                        the core of my applications. I have since used TailwindCSS in all of my new
+                                        projects.
+                                    </p>
+
+                                </article>
+
                                 <article class="group relative flex flex-col items-start">
                                     <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
                                         <div
@@ -173,7 +235,7 @@
                                         class="relative z-10 order-first mb-3 flex items-center text-sm text-orange-400 dark:text-zinc-500 pl-3.5"
                                         datetime="2022-09-02"><span class="absolute inset-y-0 left-0 flex items-center"
                                                                     aria-hidden="true"><span
-                                                class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span></span>February
+                                                class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span></span>January
                                         2, 2022
                                     </time>
                                     <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
@@ -182,30 +244,7 @@
                                         establishing myself as a go-to resource for new business opportunities.
                                     </p>
                                 </article>
-                                <article class="group relative flex flex-col items-start">
-                                    <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-                                        <div
-                                            class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
-                                        <a href="https://tailwindcss.com/" target="_blank"><span
-                                                class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span><span
-                                                class="relative z-10">Rewriting interfaces with TailwindCSS</span></a>
-                                    </h2>
-                                    <time
-                                        class="relative z-10 order-first mb-3 flex items-center text-sm text-orange-400 dark:text-zinc-500 pl-3.5"
-                                        datetime="2022-07-14"><span class="absolute inset-y-0 left-0 flex items-center"
-                                                                    aria-hidden="true"><span
-                                                class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span></span>January
-                                        14, 2022
-                                    </time>
-                                    <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-                                        When I first came across <a href="https://tailwindcss.com/" target="_blank">TailwindCSS</a>,
-                                        I was amazed by the framework's simplicity. I rewrote my interfaces in a matter
-                                        of minutes with efficiency and maintainability, leaving me more time to focus on
-                                        the core of my applications. I have since used TailwindCSS in all of my new
-                                        projects.
-                                    </p>
 
-                                </article>
                             </div>
                             <div class="space-y-10 lg:pl-16 xl:pl-24">
                                 <div class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
