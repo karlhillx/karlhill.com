2d1252d8e1be5ddb6a4a1628d099d4bb848ef926
Karl Hill <karl.hill@nasa.gov>
2024-06-10 16:39:12 -0400

    Fix user subscribed mailable.

--- app/Mail/UserSubscribed.php
+++ app/Mail/UserSubscribed.php
@@ -2,13 +2,10 @@
 
 namespace App\Mail;
 
-use Faker\Provider\Address;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Attachment;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class UserSubscribed extends Mailable
@@ -17,6 +14,8 @@ class UserSubscribed extends Mailable
 
     /**
      * Create a new message instance.
+     *
+     * @return void
      */
     public function __construct()
     {
@@ -24,32 +23,18 @@ class UserSubscribed extends Mailable
     }
 
     /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(from: new Address('karlhillx@gmail.com', 'Karl Hill'), replyTo: [
-            new Address('karlhillx@gmail.com', 'Karl Hill'),
-        ], subject: 'Thank you for subscribing to our newsletter', );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(view: 'mail.user.subscribed', with: [
-            'text' => strip_tags(Inspiring::quote()),
-        ], );
-    }
-
-    /**
-     * Get the attachments for the message.
+     * Build the message.
      *
-     * @return array<int, Attachment>
+     * @return $this
      */
-    public function attachments(): array
+    public function build()
     {
-        return [];
+        return $this->from('karlhillx@gmail.com', 'Karl Hill')
+            ->replyTo('karlhillx@gmail.com', 'Karl Hill')
+            ->subject('Thank you for subscribing to our newsletter')
+            ->view('mail.user.subscribed')
+            ->with([
+                'text' => strip_tags(Inspiring::quote()),
+            ]);
     }
 }
--- resources/views/mail/user/subscribed.blade.php
+++ resources/views/mail/user/subscribed.blade.php
@@ -1,12 +1,15 @@
-<x-mail::message>
-# Introduction
-
-The body of your message.
-
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
-
-Thanks,<br>
-{{ config('app.name') }}
-</x-mail::message>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subscription Confirmation</title>
+</head>
+<body>
+<p>Dear Subscriber,</p>
+<p>Thank you for subscribing to our newsletter. Here's an inspiring quote for you:</p>
+<blockquote>{{ $text }}</blockquote>
+<p>Best regards,</p>
+<p>Karl Hill</p>
+</body>
+</html>
