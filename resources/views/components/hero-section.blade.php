c67af8693b88222a97221b07793871167b5428b0
Karl Hill <karl.hill@nasa.gov>
2024-11-25 01:18:10 -0500

    Style and component improvements.

---
+++ resources/views/components/hero-section.blade.php
@@ -0,0 +1,36 @@
+<div class="sm:px-8 mt-4 pb-10">
+    <div class="mx-auto max-w-7xl lg:px-8">
+        <div class="relative px-4 sm:px-8 lg:px-12">
+            <div class="mx-auto max-w-2xl lg:max-w-5xl">
+                <div class="max-w-2xl">
+                    <h1 class="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                        Software engineer, volunteer, and musician.
+                    </h1>
+
+                    <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+                        I'm Karl, a full-stack software engineer at
+                        <a href="https://www.nasa.gov/goddard/" class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300">
+                            NASA Goddard Space Flight Center
+                        </a>.
+                        I am responsible for building and maintaining some of the agency's best internal and external web applications.
+                        Besides working for NASA, I volunteer for
+                        <a href="https://americanart.si.edu/visit/renwick" target="_blank" class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300">
+                            The Renwick Gallery
+                        </a>,
+                        a branch museum of the
+                        <a href="https://americanart.si.edu/" target="_blank" class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300">
+                            Smithsonian American Art Museum
+                        </a>.
+                        I also volunteer for several other non-profit organizations. In my free time, I enjoy playing music.
+                    </p>
+
+                    <div class="mt-6 flex gap-6">
+                        <x-social-link href="https://twitter.com/karl_hill" label="Follow on Twitter" icon="twitter" />
+                        <x-social-link href="https://github.com/karlhillx" label="Follow on GitHub" icon="github" />
+                        <x-social-link href="https://www.linkedin.com/in/khill/" label="Follow on LinkedIn" icon="linkedin" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PortfolioController;
+
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
-Route::get('portfolio', [WelcomeController::class, 'portfolio'])->name('portfolio');
-Route::get('house', [WelcomeController::class, 'house'])->name('house');
+Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
+Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');Route::get('house', [WelcomeController::class, 'house'])->name('house');
 Route::post('contact', [ContactController::class, 'store'])->name('contact.post');
