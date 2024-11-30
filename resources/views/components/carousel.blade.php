c4cc53ab50f6724bb47c666e24d925cf7637eafd
Karl Hill <karl.hill@nasa.gov>
2024-11-30 18:45:44 -0500

    Separate out into x-components.

---
+++ resources/views/components/carousel.blade.php
@@ -0,0 +1,46 @@
+<div class="sm:mt-20">
+    <div class="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+        <div
+            class="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl -rotate-2">
+            <a href="https://earthobservatory.nasa.gov" target="_blank">
+                <img alt="" sizes="(min-width: 640px) 18rem, 11rem"
+                     src="/img/small-earth-observatory.png"
+                     width="3936" height="2624" decoding="async" data-nimg="future"
+                     class="absolute inset-0 h-full w-full object-cover" loading="lazy"
+                     style="color:transparent">
+            </a>
+        </div>
+        <div
+            class="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl -rotate-2">
+            <img alt="" sizes="(min-width: 640px) 18rem, 11rem"
+                 src="/img/small-informeddna.png"
+                 width="4240" height="2384" decoding="async" data-nimg="future"
+                 class="absolute inset-0 h-full w-full object-cover" loading="lazy"
+                 style="color:transparent">
+        </div>
+        <div
+            class="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl rotate-2">
+            <img alt="" sizes="(min-width: 640px) 18rem, 11rem"
+                 src="/img/small-direct-readout.png"
+                 width="3744" height="5616" decoding="async" data-nimg="future"
+                 class="absolute inset-0 h-full w-full object-cover" loading="lazy"
+                 style="color:transparent">
+        </div>
+        <div
+            class="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl rotate-2">
+            <a href="#"><img alt="" sizes="(min-width: 640px) 18rem, 11rem"
+                             src="/img/small-flood.png"
+                             width="5760" height="3840" decoding="async" data-nimg="future"
+                             class="absolute inset-0 h-full w-full object-cover" loading="lazy"
+                             style="color:transparent"></a>
+        </div>
+        <div
+            class="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl rotate-2">
+            <img alt="" sizes="(min-width: 640px) 18rem, 11rem"
+                 src="/img/small-mci-verizon.png"
+                 width="2400" height="3000" decoding="async" data-nimg="future"
+                 class="absolute inset-0 h-full w-full object-cover" loading="lazy"
+                 style="color:transparent">
+        </div>
+    </div>
+</div>
