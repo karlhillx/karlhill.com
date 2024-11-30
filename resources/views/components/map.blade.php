ce18b66035715b663b7dc7da8d1a0d0073caa4ef
Karl Hill <karl.hill@nasa.gov>
2024-11-30 18:09:42 -0500

    Update map component styles and functionality.

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -1,9 +1,13 @@
-<section id="map">
-    <div id="map" class="bg-gray-200 dark:bg-gray-900 border-t border-yellow-400">
-        <iframe class="w-full" style="height: 45vh;"
-                loading="lazy"
-                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA1U9kPUx3SN0u33kKiaCSXl7plnfA3y8Q&q=Brightwood+Park,+Washington,+DC+20011"
-                allowfullscreen>
+<section id="map" class="bg-gray-200 dark:bg-gray-900 border-t border-yellow-400">
+    <div class="max-w-8xl mx-auto">
+        <iframe
+            class="w-full transition-all duration-300 hover:opacity-95 filter saturate-[0.85] hover:saturate-100"
+            style="height: 50vh"
+            loading="lazy"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA1U9kPUx3SN0u33kKiaCSXl7plnfA3y8Q&q=Brightwood+Park,+Washington,+DC+20011&zoom=14"
+            allowfullscreen
+            referrerpolicy="no-referrer-when-downgrade"
+            title="Google Maps - Brightwood Park Location">
         </iframe>
     </div>
 </section>
