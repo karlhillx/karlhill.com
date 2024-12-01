1f8b97b0290c1a4ac39c08f7b65585ff587a7d24
Karl Hill <karl.hill@nasa.gov>
2024-12-01 18:38:20 -0500

    Fix controllers.

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+
 class PortfolioController extends Controller
 {
-    public function index()
+    /**
+     * Display the portfolio page
+     */
+    public function index(): View
     {
         return view('portfolio');
     }
--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -7,20 +7,10 @@ use Illuminate\Contracts\View\View;
 class WelcomeController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display the welcome page
      */
     public function index(): View
     {
         return view('welcome');
-    }
-
-    public function portfolio(): View
-    {
-        return view('portfolio');
-    }
-
-    public function house(): View
-    {
-        return view('house');
     }
 }
