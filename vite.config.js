e20f9c27124ec2eca58cdaed4814a1c4254f9225
Karl Hill <karl.hill@nasa.gov>
2024-12-26 01:22:03 -0500

    Switch to using a dynamic import() statement instead of require().

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import postcss from './postcss.config.mjs';
 
-export default defineConfig({
-    css: {
-        postcss,
-    },
-  plugins: [
-      laravel({
-          input: [
-              'resources/css/app.css',
-              'resources/js/app.js',
-          ],
-          refresh: true,
-      }),
-  ]
+export default defineConfig(async () => {
+    const { default: laravel } = await import('laravel-vite-plugin');
+
+    return {
+        plugins: [
+            laravel({ // Now you can use the imported laravel plugin
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+    };
 });
