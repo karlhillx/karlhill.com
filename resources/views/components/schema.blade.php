dfc86ab898f4357e0d568451262e1246b8fe22c1
Karl Hill <karlhillx@gmail.com>
2025-09-14 01:28:12 -0400

    Prevent Blade from interpreting '@' as directives.

--- resources/views/components/schema.blade.php
+++ resources/views/components/schema.blade.php
@@ -1,13 +1,12 @@
 <script type="application/ld+json">
 {
-  "@context": "https://schema.org",
-  "@type": "Person",
-  "@id": "https://karlhill.com/#person",
+  "@@context": "https://schema.org",
+  "@@type": "Person",
   "name": "Karl Hill",
   "jobTitle": "Staff Aerospace Software Engineer",
   "url": "https://karlhill.com",
   "worksFor": {
-    "@type": "Organization",
+    "@@type": "Organization",
     "name": "Jacobs"
   },
   "sameAs": [
